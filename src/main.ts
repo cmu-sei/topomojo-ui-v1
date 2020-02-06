2ea4b25af62dae3be5fff8744e9d30eb5615070d
Jeff Mattson <jam@Jeffs-MacBook-Pro.local>
2020-02-06 16:40:13 -0500

    anuglar material v9

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 // Released under a 3 Clause BSD-style license. See LICENSE.md in the project root for license information.
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
